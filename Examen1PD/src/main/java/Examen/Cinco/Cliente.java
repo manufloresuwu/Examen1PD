47e4a2b8ed57b5cc603565629fe6a402a24a8af3
manufloresuwu <60782110+manufloresuwu@users.noreply.github.com>
2021-05-31 09:22:59 -0400

    Primer Parcial

--- Examen1PD/src/main/java/Examen/Cinco/Cliente.java
+++ Examen1PD/src/main/java/Examen/Cinco/Cliente.java
@@ -7,7 +7,7 @@ public class Cliente {
         String op="1 sumar 2 restar 2 sumar 1 sumar 1";
         Interpreter interpreterOperaciones = new Interpreter(op);
         System.out.println("Operacion: " + op);
-        System.out.println("Resultado: "+interpreterOperaciones.interpretar());
+        System.out.println("Resultado: " + interpreterOperaciones.interpretar());
 
     }
 
